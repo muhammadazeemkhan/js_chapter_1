8abd6bd44fca478b0af34b26fce7aa68f5b88cba
muhammadazeemkhan <121564168+muhammadazeemkhan@users.noreply.github.com>
2023-05-06 17:46:25 +0500

    Add files via upload
    completed

---
+++ app.js
@@ -0,0 +1,18 @@
+function question_1(){
+    var friendName = prompt("Enter your friend name");
+    alert("Hello "+ friendName)
+}
+
+function question_2(){
+    alert("Error! please enter valid password")
+}
+
+
+function question_3(){
+    alert("Welcome to JS land...."+"\n"+"Happy Coding!")
+}
+
+function question_4(){
+    alert("Welcome to JS land")
+    alert("Happy Coding!    ")
+}
